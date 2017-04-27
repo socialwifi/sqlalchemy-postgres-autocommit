9aa349044a6e1e5905778903f76227dd48acca12
Jakub Skiepko <jakub.skiepko@socialwifi.com>
2017-04-27 16:34:50 +0200

    Setup database connection in separate call.
    Change-Id: I02948073a2627525d8bea70b4c605e59a498e748

--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -2,7 +2,8 @@ from sqlalchemy_postgres_autocommit import Database
 
 
 def test_sanity():
-    db = Database(database_url='postgresql://admin:admin@localhost:5432/test')
+    db = Database()
+    db.connect(database_url='postgresql://admin:admin@localhost:5432/test')
     assert db.engine.dialect.isolation_level == 'AUTOCOMMIT'
     s = db.Session()
     assert s.autocommit is True
