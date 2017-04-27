d2df407d2db36e515ca30f44a2a9cc5be290e2a5
Jakub Gocławski <jakub@goclawski.net>
2017-04-27 12:25:22 +0200

    Add Travis.
    There are no meaningful tests yet.

---
+++ tests/test_sanity.py
@@ -0,0 +1,8 @@
+from sqlalchemy_postgres_autocommit import Database
+
+
+def test_sanity():
+    db = Database(database_url='postgresql://admin:admin@localhost:5432/test')
+    assert db.engine.dialect.isolation_level == 'AUTOCOMMIT'
+    s = db.Session()
+    assert s.autocommit is True
